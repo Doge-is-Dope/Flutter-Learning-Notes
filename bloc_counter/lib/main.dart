4d39847961da87134265b2964907713022b8537e
doge-is-dope <maple3312936@outlook.com>
2021-07-26 00:54:27 +0800

    feat: add bloc counter

---
+++ bloc_counter/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:bloc_counter/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: "Bloc Counter",
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MyHomePage(
+          title: "a",
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("Incremented!"),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                } else if (state.wasIncremented == false) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("Decremented!"),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  "${state.counterValue}",
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () => context.read<CounterCubit>().decrement(),
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () => context.read<CounterCubit>().increment(),
+                  child: Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
